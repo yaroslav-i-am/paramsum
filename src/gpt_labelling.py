43c34f279298766e68820cd4cf22b58f739dd08d
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2024-05-01 22:53:09 +0300

    feat & refactor: Add Hydra-conf for gpt_labelling & minor refactor for full pipeline

--- src/gpt_labelling.py
+++ src/gpt_labelling.py
@@ -6,11 +6,21 @@ from loguru import logger
 from tqdm import trange
 from gpt_requests import gpt_async_request, gpt_answer
 from utils import get_topics
-from config import logs_dir
 from pathlib import Path
 
+import hydra
+from hydra import compose, initialize
+from omegaconf import OmegaConf
+
+_version = '1.1'
+_job_name = "gpt_markup_recv"
+with initialize(version_base=_version, config_path="../cfg", job_name=_job_name):
+    cfg = compose(config_name="config.yaml")
+
+print(OmegaConf.to_yaml(cfg))
+
 logger.add(
-    Path(logs_dir, 'gpt_requests.log'),
+    Path(cfg['logging_dir'], 'gpt_requests.log'),
     rotation='100 MB',
     encoding='UTF-8'
 )
@@ -18,7 +28,7 @@ logger.add(
 id_filename = '../async_ids.txt'
 id_file = open(id_filename, 'wt')
 
-labelled_aspects = pd.read_csv(input('Enter filename of labelled aspects: '))
+labelled_aspects = pd.read_csv(cfg['gold_markup_path'])
 print(labelled_aspects)
 
 aspect_adapter = {v: v for v in get_topics()}
@@ -26,14 +36,14 @@ aspect_adapter['описание игры актёров'] = 'описание �
 
 
 # for i in trange(labelled_aspects.shape[0]):
-for i in trange(20):
+for i in trange(100):
     row = labelled_aspects.iloc[i, :]
     aspect = row['aspect']
     review = row['review']
 
     response: Dict = gpt_async_request(aspect_adapter[aspect], review, logger)
     id_file.write(response.get('id') + '\n')
-    sleep(3)
+    sleep(1)
 
 id_file.close()
 
@@ -41,13 +51,12 @@ logger.debug('Waiting for the answer...')
 sleep(60)
 
 gpt_responses = []
-gpt_markup_filename = '../data/working_dir/gpt_markup.csv'
 
 with open(id_filename, 'rt') as id_file:
     for req_id in id_file.readlines():
         answer: Dict = gpt_answer(req_id.strip(), logger)
         gpt_responses.append(answer)
 
-gpt_markup_df = pd.DataFrame()
+gpt_markup_df = pd.read_csv(cfg['gold_markup_path'])
 gpt_markup_df['gpt_markup'] = gpt_responses
-gpt_markup_df.to_csv(gpt_markup_filename, index=False)
+gpt_markup_df.to_csv(cfg['gpt_markup_path'], index=False)
