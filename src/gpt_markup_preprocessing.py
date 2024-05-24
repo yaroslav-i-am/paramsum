f122cadb79679962700561ec8ce5c2b5504c6cb6
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2024-05-24 14:15:49 +0300

    feat: Add conditional final preprocessing

--- src/gpt_markup_preprocessing.py
+++ src/gpt_markup_preprocessing.py
@@ -6,22 +6,7 @@ import pandas as pd
 from hydra import compose, initialize
 from loguru import logger
 from omegaconf import OmegaConf
-
-_version = '1.1'
-_job_name = "gpt_markup_preprocessing_job"
-with initialize(version_base=_version, config_path="../cfg", job_name=_job_name):
-    cfg = compose(config_name="config.yaml")
-
-print(OmegaConf.to_yaml(cfg))
-
-logger.add(
-    Path(cfg['logging_dir'], 'gpt_markup_preprocessing.log'),
-    rotation='100 MB',
-    encoding='UTF-8'
-)
-
-gpt_markup = pd.read_csv(cfg['silver_markup_path'])
-logger.debug(f'{gpt_markup.shape = }')
+from tqdm.auto import tqdm
 
 
 def process_gpt_markup(line: str, logger):
@@ -41,6 +26,9 @@ def process_gpt_markup(line: str, logger):
         if not line:
             line = ['<SOME_ASPECT>']
 
+        # if 'inference' in config_name and line == ['<SOME_ASPECT>']:
+        #     line = ['-']
+
         # line = set(line)
 
     logger.debug('FINAL LINE')
@@ -50,5 +38,35 @@ def process_gpt_markup(line: str, logger):
     return line
 
 
-gpt_markup['gpt_markup_preprocessed'] = gpt_markup['gpt_markup'].apply(process_gpt_markup, args=(logger,))
-gpt_markup.to_csv(cfg['silver_markup_parsed_path'], index=False)
+if __name__ == '__main__':
+
+    tqdm.pandas()
+    _version = '1.1'
+    _job_name = "gpt_markup_preprocessing_job"
+    config_name = "inference_config.yaml"
+
+    with initialize(version_base=_version, config_path="../cfg", job_name=_job_name):
+        cfg = compose(config_name=config_name)
+
+    print(OmegaConf.to_yaml(cfg))
+
+    logger.add(
+        Path(cfg['logging_dir'], 'gpt_markup_preprocessing.log'),
+        rotation='100 MB',
+        encoding='UTF-8'
+    )
+    gpt_markup = pd.read_csv(cfg['silver_markup_path'])
+
+
+    logger.debug(f'{gpt_markup.shape = }')
+
+    if 'inference' in config_name:
+        gpt_markup = gpt_markup[~gpt_markup['gpt_markup'].str.contains('Давайте сменим тему?').fillna(False)]
+
+    gpt_markup['gpt_markup_preprocessed'] = gpt_markup['gpt_markup'].progress_apply(process_gpt_markup, args=(logger,))
+
+    if 'inference' in config_name:
+        gpt_markup['gpt_markup_preprocessed'] = gpt_markup['gpt_markup_preprocessed'].fillna("['-']")
+        gpt_markup = gpt_markup[gpt_markup['gpt_markup_preprocessed'].astype('str') != "['<SOME_ASPECT>']"]
+
+    gpt_markup.to_csv(cfg['silver_markup_parsed_path'], index=False)
